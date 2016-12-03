48d9e41aa41a06518640f14959ce7c4990858f3c
章斌斌 <zhangbinbin@zhangbinbindeMac.local>
2016-12-03 11:14:37 +0800

    Initial Commit

---
+++ AppForGirlFriend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppForGirlFriend
+//
+//  Created by 章斌斌 on 2016/12/3.
+//  Copyright © 2016年 章斌斌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
